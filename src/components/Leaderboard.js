92b1fffe462af393280661ec16e0ce73c4d98f89
Chandan <chandddann14@gmail.com>
2023-04-15 18:44:59 +0530

    added leaderboard extraa

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,23 +1,69 @@
 import React from "react";
 
 const leaderboardData = [
+    {
+      username: "EmilyDavis",
+      points: 2347,
+      iconUrl: "https://randomuser.me/api/portraits/women/2.jpg"
+    },
+    {
+        "username": "AbbySmith",
+        "points": 1874,
+        "iconUrl": "https://randomuser.me/api/portraits/women/30.jpg"
+      },
+    {
+      username: "SaraSmith",
+      points: 1572,
+      iconUrl: "https://randomuser.me/api/portraits/women/1.jpg"
+    },
+    {
+      "username": "JuliaGreen",
+      "points": 1489,
+      "iconUrl": "https://randomuser.me/api/portraits/women/67.jpg"
+    },
   {
     username: "JohnDoe",
-    points: 120,
+    points: 1200,
     iconUrl: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
+    },
   {
-    username: "JaneDoe",
-    points: 80,
-    iconUrl: "https://randomuser.me/api/portraits/women/32.jpg",
-  },
+      username: "RobertGarcia",
+      points: 1032,
+      iconUrl: "https://randomuser.me/api/portraits/men/12.jpg"
+    },
+    {
+      "username": "EmilyDavis",
+      "points": 938,
+      "iconUrl": "https://randomuser.me/api/portraits/women/82.jpg"
+    },
+    {
+      username: "AlexLee",
+      points: 827,
+      iconUrl: "https://randomuser.me/api/portraits/men/31.jpg"
+    },
+    {
+        username: "JasmineKim",
+        points: 501,
+        iconUrl: "https://randomuser.me/api/portraits/women/3.jpg"
+    },
+    {
+      username: "DavidJones",
+      points: 341,
+      iconUrl: "https://randomuser.me/api/portraits/men/45.jpg"
+    },
+      {
+        "username": "TomJohnson",
+        "points": 222,
+        "iconUrl": "https://randomuser.me/api/portraits/men/24.jpg"
+      },
+   
   // Add more entries here
 ];
 
 function Leaderboard() {
   return (
     <div className="bg-purple-500 p-4 rounded-lg">
-      <h2 className="text-white text-lg font-bold mb-2">Leaderboard</h2>
+      <h2 className="text-white text-[50px] font-bold mb-10">Leader Board</h2>
       {leaderboardData.map((entry, index) => (
         <div
           key={index}
